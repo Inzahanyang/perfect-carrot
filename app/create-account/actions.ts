200a4fb56ae969154595dab79e01f78082209cff
inzahanyang <inzahanyang@gmail.com>
2024-06-10 21:24:10 +0900

    add token schema

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,7 +1,15 @@
 "use server";
-
-import { PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
+import bcrypt from "bcrypt";
+import {
+  PASSWORD_MIN_LENGTH,
+  PASSWORD_REGEX,
+  PASSWORD_REGEX_ERROR,
+} from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const checkUsername = (username: string) => !username.includes("potato");
 const checkPassword = ({
@@ -12,6 +20,25 @@ const checkPassword = ({
   confirm_password: string;
 }) => password === confirm_password;
 
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: { username },
+    select: {
+      id: true,
+    },
+  });
+  // return user ? false : true
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: { email },
+    select: { id: true },
+  });
+  return !Boolean(user);
+};
+
 const formSchema = z
   .object({
     username: z
@@ -21,11 +48,17 @@ const formSchema = z
       })
       .trim()
       .toLowerCase()
-      .transform((username) => `🥕 ${username} 🥕`)
-      .refine(checkUsername, "No potatoes allowed"),
-    email: z.string().email().toLowerCase(),
-    password: z.string().min(10).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
-    confirm_password: z.string().min(10),
+      // .transform((username) => `🥕 ${username} 🥕`)
+      .refine(checkUsername, "No potatoes allowed")
+      .refine(checkUniqueUsername, "이미 사용중인 유저네임 입니다."),
+    email: z
+      .string()
+      .email()
+      .toLowerCase()
+      .refine(checkUniqueEmail, "이미 사용중인 이메일 입니다."),
+    password: z.string().min(PASSWORD_MIN_LENGTH),
+    // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+    confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
   })
   .refine(checkPassword, {
     message: "설정한 비밀번호가 일치하지 않네요. 같은 비밀번호를 입력해주세요.",
@@ -40,11 +73,33 @@ export async function createAccount(prevState: any, formData: FormData) {
     confirm_password: formData.get("confirm_password"),
   };
 
-  const result = formSchema.safeParse(data);
-  console.log(result.error?.flatten());
+  const result = await formSchema.safeParseAsync(data);
+
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    // hash password
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    // save the user to db
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    // log the user in
+    const cookie = await getIronSession(cookies(), {
+      cookieName: "nice cookie",
+      password: process.env.COOKIE_PASSWORD!,
+    });
+    // @ts-ignore
+    cookie.id = user.id;
+    await cookie.save();
+    redirect("/profile");
   }
 }
